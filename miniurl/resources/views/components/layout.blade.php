2c6616de8f7a846c6ec6f51a541b5a41aa87eaec
Janix-dev <csomosjanos08@gmail.com>
2026-04-09 17:08:45 +0200

    Shortener Page created

---
+++ miniurl/resources/views/components/layout.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $heading ?? 'Alapértelmezett Cím' }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+    <div class="min-h-full">
+        <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex h-16 items-center justify-between">
+                    <div class="flex items-center">
+                        <div class="shrink-0">
+                            <img class="size-8" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Logo">
+                        </div>
+                        
+                        <div class="ml-10 flex items-baseline space-x-4">
+                            <x-nav-link href="/" :active="request()->is('/')">Shortener</x-nav-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <header class="bg-white shadow-sm">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+            </div>
+        </header>
+
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+
+    <x-footer>
+        <x-slot:pagetitle>{{ $pagetitle ?? 'MiniURL' }}</x-slot:pagetitle>
+        <x-slot:idezet>{{ $idezet ?? 'Alapértelmezett idézet' }}</x-slot:idezet>
+    </x-footer>
+
+</body>
+</html>
