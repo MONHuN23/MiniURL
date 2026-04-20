9e37902c8813b807579074159da2d0a9758a621b
Janix-dev <csomosjanos08@gmail.com>
2026-04-20 15:03:57 +0200

    Improved Link controller/Model logic

--- miniurl/app/Http/Controllers/LinkController.php
+++ miniurl/app/Http/Controllers/LinkController.php
@@ -9,22 +9,14 @@ use App\Models\Link;
 class LinkController extends Controller
 {
     public function createLink(Request $request) {
-        $userInfo = $request->validate([
+        $urlInfo = $request->validate([
             'url' => ['required', 'url'],
             'custom_url' => ['nullable', 'string', 'min:3', 'max:30', 'unique:links,short_url']
-            ]);
-
-        if (empty($userInfo['custom_url'])) {
-            $shortUrl = Str::random(8);
-        } else {
-            $shortUrl = $userInfo['custom_url'];
-        }
-        
-        Link::create([
-            'original_url' => $userInfo['url'],
-            'short_url' => $shortUrl
         ]);
+
         
-        return response("A rövidített url-ed: " . url('/') . "/" . $shortUrl);
+        $link = Link::createWithShortUrl($urlInfo);
+
+        return response("A rövidített url-ed: " . url('/') . "/" . $link->short_url);
     }
 }
--- miniurl/app/Models/Link.php
+++ miniurl/app/Models/Link.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Link extends Model
 {
@@ -13,4 +14,13 @@ class Link extends Model
     protected $primaryKey = 'id';
     protected $keyType = 'int';
     public $incrementing = true;
+
+    public static function createWithShortUrl(array $attributes) {
+        $shortUrl = $attributes['custom_url'] ?? Str::random(8);
+
+        return self::create([
+            'original_url' => $attributes['url'],
+            'short_url' => $shortUrl
+        ]);
+    }
 }
